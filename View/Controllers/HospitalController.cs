974ec786c8a48191adce29dfd57f2f73a1f7437f
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-07-12 17:38:49 -0300

    Feito projeto de Hospitais.

---
+++ View/Controllers/HospitalController.cs
@@ -0,0 +1,66 @@
+﻿using Model;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class HospitalController : Controller
+    {
+        private HospitalRepository repository;
+
+        public HospitalController()
+        {
+            repository = new HospitalRepository();
+        }
+        // GET: Hospital
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult obterTodos(string busca)
+        {
+            List<Hospital> hospitais = repository.ObterTodos(busca);
+            return Json(hospitais, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        // JsonResult retorna um json para a requisição
+        public JsonResult Store(Hospital hospital)
+        {
+            hospital.RegistroAtivo = true;
+            repository.Inserir(hospital);
+            // retorna no formato de json o objeto de hospital que foi persistido no banco de dados.
+            return Json(hospital);
+        }
+
+        [HttpGet]
+        [Route("apagar/{id}")]
+        public JsonResult Apagar(int id)
+        {
+            bool apagou = repository.Apagar(id);
+            return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet, Route("obterpeloid/{id}")]
+        public JsonResult ObterPeloId(int id)
+        {
+            Hospital hospital = repository.ObterPeloId(id);
+            return Json(hospital, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Hospital hospital)
+        {
+            bool alterou = repository.Alterar(hospital);
+            return Json(new { status = alterou });
+        }
+    }
+}
---
+++ View/Scripts/Hospital/Cadastro.js
@@ -0,0 +1,167 @@
+﻿// jquery
+$(function () {
+
+    $id = -1;
+
+
+    $(".table").on("click", ".botao-editar", function () {
+        $id = $(this).data("id");
+        $.ajax({
+            url: '/hospital/obterpeloid/' + $id,
+            method: 'get',
+            success: function (data) {
+                $id = data.Id;
+                $("#campo-razao-social").val(data.RazaoSocial);
+                $("#campo-faturamento").val(data.Faturamento);
+                $("#campo-cnpj").val(data.Cnpj);
+                $("#modalCadastroHospital").modal("show");
+            }
+        })
+    })
+    $("#campo-pesquisa").on('keyup', function (e) {
+        // 13 == tecla Enter do teclado
+        if (e.keyCode == 13) {
+            obterTodos();
+        }
+    });
+    function obterTodos() {
+        $busca = $("#campo-pesquisa").val();
+        $("#lista-hospitais").empty();
+        $.ajax({
+            url: '/hospital/obtertodos',
+            method: 'get',
+            data: {
+                busca : $busca
+            },
+            success: function (data) {
+
+                for (var i = 0; i < data.length; i++) {
+                    var dado = data[i];
+
+                    var linha = document.createElement("tr");
+                    var colunaCodigo = document.createElement("td");
+                    colunaCodigo.innerHTML = dado.Id;
+
+                    var colunaRazaoSocial = document.createElement("td");
+                    colunaRazaoSocial.innerHTML = dado.RazaoSocial;
+
+                    var colunaCNPJ = document.createElement("td");
+                    colunaCNPJ.innerHTML = dado.Cnpj;
+
+                    var colunaAcao = document.createElement("td");
+                    var botaoEditar = document.createElement("button");
+                    botaoEditar.innerHTML = "Editar";
+                    botaoEditar.classList.add("btn", "btn-primary", "mr-3", "botao-editar");
+                    botaoEditar.innerHTML = "<i class=\"fas fa-pen\"></i> Editar";
+                    botaoEditar.setAttribute("data-id", dado.Id);
+
+                    var botaoApagar = document.createElement("button");
+                    botaoApagar.innerHTML = "<i class=\"fas fa-trash\"></i> Apagar";
+                    botaoApagar.classList.add("btn", "btn-danger", "botao-apagar");
+                    botaoApagar.setAttribute("data-id", dado.Id);
+
+
+                    colunaAcao.appendChild(botaoEditar);
+                    colunaAcao.appendChild(botaoApagar);
+
+                    linha.appendChild(colunaCodigo);
+                    linha.appendChild(colunaRazaoSocial);
+                    linha.appendChild(colunaCNPJ);
+                    linha.appendChild(colunaAcao);
+                    document.getElementById("lista-hospitais").appendChild(linha);
+                }
+            },
+            error: function (data) {
+                alert("DEU RUIM");
+            }
+        })
+    }
+
+    $("#hospital-botao-salvar").on("click", function () {
+        if ($id == -1) {
+            inserir();
+        } else {
+            alterar();
+        }
+    });
+
+    function alterar() {
+        $nome = $("#campo-razao-social").val();
+        $faturamento = $("#campo-faturamento").val();
+        $cnpj = $("#campo-cnpj").val();
+        $particular = $("#campo-privado").prop("checked");
+        $.ajax({
+            method: "post",
+            url: "/hospital/update",
+            data: {
+                RazaoSocial: $nome,
+                Faturamento: $faturamento,
+                Cnpj: $cnpj,
+                Particular: $particular,
+                Id: $id
+            },
+            success: function (data) {
+                $id = -1;
+                obterTodos();
+                $("#modalCadastroHospital").modal("hide")
+                obterTodos();
+            },
+            error: function (data) {
+                console.log("ERRO");
+            }
+        })
+
+    }
+
+    function inserir() {
+        $nome = $("#campo-razao-social").val();
+        $faturamento = $("#campo-faturamento").val();
+        $cnpj = $("#campo-cnpj").val();
+        $particular = $("#campo-privado").prop("checked");
+        $.ajax({
+            method: "post",
+            url: "/hospital/store",
+            data: {
+                RazaoSocial: $nome,
+                Faturamento: $faturamento,
+                Cnpj: $cnpj,
+                Particular: $particular
+            },
+            success: function (data) {
+                $id = -1;
+                obterTodos();
+                $("#modalCadastroHospital").modal("hide")
+                obterTodos();
+            },
+            error: function (data) {
+                console.log("ERRO");
+            }
+        })
+    }
+
+
+    function limparCampos() {
+        $("#campo-razao-social").val("");
+        $("#campo-cnpj").val("");
+        $("#campo-faturamento").val("");
+        $("#campo-privado").prop("checked", false);
+
+    }
+
+    $(".table").on("click", ".botao-apagar", function () {
+        $id = $(this).data("id");
+        $.ajax({
+            url: '/hospital/apagar/' + $id,
+            method: 'get',
+            success: function (data) {
+                obterTodos();
+            },
+            error: function (data) {
+                console.log('Deu ruim filhao');
+            }
+        });
+    });
+
+    obterTodos();
+});
+
